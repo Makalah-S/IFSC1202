426fc51296961bebc320602c2432c929b84989c1
Makalah-S <156933087+Makalah-S@users.noreply.github.com>
2024-01-24 21:15:13 +0000

    Updating Some Things

--- Next Whole Number.py
+++ Next Whole Number.py
@@ -1,5 +1,4 @@
 x = float(input("Next Whole Number: "))
 if x%1 != 0:
     x = x//1 + 1
-
 print(x)
---
+++ Unit 2/02.01 - Last Digit.py
@@ -0,0 +1,6 @@
+x = int(input("Number: "))
+ones = x % 10
+tens = x // 10
+
+print("Ones Digit: {}" .format(ones))
+print("Tens Digit: {}" .format(tens))
